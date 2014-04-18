098ac23190c812552fe8474466a55e32941534bb
Bproth <bproth12@gmail.com>
2014-04-18 15:12:14 -0500

    Heap sorting

--- Lab11/src/MaxHeap.java
+++ Lab11/src/MaxHeap.java
@@ -3,25 +3,48 @@ public class MaxHeap<T extends Comparable<T>> extends BinaryTree<T>
 {
 	private TreeNode<T> root;
 
+	private void lSwap(TreeNode<T> cur, TreeNode<T> trail)
+	{
+		cur.getLeft().setRight(cur.getRight());
+		cur.getLeft().setLeft(cur);
+		cur = cur.getLeft();
+		trail.setLeft(cur);
+	}
+	
+	private void rSwap(TreeNode<T> cur, TreeNode<T> trail)
+	{
+		cur.getRight().setLeft(cur.getLeft());
+		cur.getRight().setRight(cur);
+		cur = cur.getRight();
+		trail.setLeft(cur);
+	}
+	
 	@Override
 	public boolean add(T obj)
 	{
-		if (root.getData().compareTo(obj) < 0)
+		TreeNode<T> cur = root;
+		while(cur.getLeft() != null)
 		{
-			
-			return true;
+			cur = cur.getLeft();
 		}
-		else
-		{
-			return super.add(obj);
-		}
+		cur.setLeft(new TreeNode<T>(obj));
+		heapify();
+		return true;
 	}
 	
 	public void heapify()
 	{
-		if (root.getData().compareTo(root.getLeft().getData()) < 0)
+		TreeNode<T> cur = root;
+		TreeNode<T> trail = new TreeNode<T>();
+		while(cur.getLeft() != null)
 		{
-			
+			trail = cur;
+			cur = cur.getLeft();
 		}
+		if (cur.getLeft().getData().compareTo(cur.getData()) > 0)
+		{
+			swap(cur, trail);
+		}
+		if (cur.getRight().getData().compareTo(cur.getData()) > 0)
 	}
 }
